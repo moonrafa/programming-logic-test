db336a818c0cc4b5182da140ad3a5f7b4cfeb8c6
moonrafa <rrafasrodrigues@gmail.com>
2022-12-01 17:40:57 -0300

    add solution and testing for the first question

---
+++ pergunta1/pergunta1.py
@@ -0,0 +1,16 @@
+# Dado o Array de inteiros abaixo, ordene-o de tal forma que os números “1” estejam à esquerda. Os itens devem ser modificados no lugar, ou seja, você não ira trocar posições e sim colocar os números “1” no inicio do Array.
+
+def oneFirst(lista):
+  # Primeiro, conta-se a quantidade de números 1 na lista
+  amountOfOnes = lista.count(1)
+
+  # Iremos então tirá-los das suas posições
+  while 1 in lista:
+    lista.remove(1)
+    
+  # E colocar-los no inicio da lista
+  for i in range( 0, amountOfOnes):
+    # se amountOfOnes = 4
+    # i = 0, 1, 2, 3 
+    lista.insert(i, 1)
+  return lista
---
+++ pergunta1/pergunta1.test.py
@@ -0,0 +1,16 @@
+import unittest
+from pergunta1 import oneFirst
+
+class Test(unittest.TestCase):
+
+  def testPergunta1(self):
+    self.assertEqual(oneFirst([2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]), [1, 1, 1, 1, 2, 5, 2, 5, 2, 7, 9, 13, 127, 21])
+
+  def testMostlyOnes(self):
+    self.assertEqual(oneFirst([1, 2, 1, 1, 1]), [1, 1, 1, 1, 2])
+
+  def testAllOnes(self):
+    self.assertEqual(oneFirst([1, 1, 1, 1, 1, 1]), [1, 1, 1, 1, 1, 1])  
+    
+if __name__ == '__main__':
+    unittest.main()
